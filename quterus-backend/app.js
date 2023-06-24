035a478616992306ddfdcb6af9f8babe9368b95a
Trang Tran <trantrang181197@gmail.com>
2023-06-24 11:32:01 -0700

    added cors to back-end

--- quterus-backend/app.js
+++ quterus-backend/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
-
+// Middlewares
+app.use(cors());
 app.use(bodyParser.json());
 
 // Import Routes
